71829823e6117703363365d5a1d3170f84433add
jinwoo22 <98975580+JINU-CHANG@users.noreply.github.com>
2024-08-15 16:03:23 +0900

    [Silver I] Title: 단지번호붙이기, Time: 176 ms, Memory: 18152 KB -BaekjoonHub

---
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
@@ -0,0 +1,66 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Main {
+
+    static int n;
+    static boolean[][] visited;
+    static int[][] map;
+    static int[] dx = {0 , 0, -1, 1};
+    static int[] dy = {-1, 1, 0, 0};
+    static int cnt = 0;
+    static List<Integer> ans = new ArrayList<>();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        n = Integer.parseInt(br.readLine());
+        map = new int[n][n];
+        visited = new boolean[n][n];
+
+        for (int i = 0; i < n; i++) {
+            String line = br.readLine();
+
+            for (int j = 0; j < n; j++) {
+                map[i][j] = line.charAt(j) - '0';
+            }
+        }
+
+        for (int y = 0; y < n; y++) {
+            for (int x = 0; x < n; x++) {
+                if (map[y][x] == 1 && !visited[y][x]) {
+                    dfs(y, x);
+                    ans.add(cnt);
+                    cnt = 0;
+                }
+            }
+        }
+
+        Collections.sort(ans);
+        sb.append(ans.size()).append("\n");
+        ans.forEach(x -> sb.append(x).append("\n"));
+
+        System.out.println(sb);
+    }
+
+    public static void dfs(int y, int x) {
+        visited[y][x] = true;
+        cnt++;
+        
+        for (int i = 0; i < 4; i++) {
+            int ny = y + dy[i];
+            int nx = x + dx[i];
+
+            if (ny < 0 || nx < 0 || nx >= n || ny >= n) continue;
+            if (visited[ny][nx]) continue;
+            if (map[ny][nx] == 0) continue;
+
+            dfs(ny, nx);
+        }
+    }
+}
