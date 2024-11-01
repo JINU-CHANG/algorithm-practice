263d462427aecc46ca28a999167bf35dfef0780c
jinwoo22 <98975580+JINU-CHANG@users.noreply.github.com>
2024-11-01 23:14:03 +0900

    [Silver IV] Title: 크림 파스타, Time: 1556 ms, Memory: 79752 KB -BaekjoonHub

---
+++ 백준/Silver/25214. 크림 파스타/크림 파스타.java
@@ -0,0 +1,52 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static int[] array;
+    static int n;
+
+    public static void main(String[] args) throws IOException {
+       BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+       StringTokenizer st = new StringTokenizer(br.readLine());
+
+       n = Integer.parseInt(st.nextToken());
+
+       array = new int[n];
+       st = new StringTokenizer(br.readLine());
+       for (int i = 0; i < n; i++) {
+           array[i] = Integer.parseInt(st.nextToken());
+       }
+
+       solve();
+    }
+
+    public static void solve() {
+        int minIdx = 0;
+        int maxIdx = 0;
+        int realMinIdx = 0;
+
+        for (int i = 0; i < n; i++) {
+            if (array[i] > array[maxIdx]) {
+                maxIdx = i;
+            }
+
+            if (array[i] < array[minIdx] && i < maxIdx) {
+                minIdx = i;
+            }
+
+            if (array[maxIdx] - array[minIdx] < array[i] - array[realMinIdx]) {
+                maxIdx = i;
+                minIdx = realMinIdx;
+            }
+
+            System.out.print(array[maxIdx] - array[minIdx] + " ");
+
+            if (array[i] < array[realMinIdx]) {
+                realMinIdx = i;
+            }
+        }
+    }
+}
