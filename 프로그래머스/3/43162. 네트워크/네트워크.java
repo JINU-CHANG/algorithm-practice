7126dc0bc4eb8aec4141a263b69d6cff8cc440b2
jinwoo22 <98975580+JINU-CHANG@users.noreply.github.com>
2025-03-07 20:37:35 +0900

    [level 3] Title: 네트워크, Time: 0.16 ms, Memory: 87.7 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -4,27 +4,20 @@ class Solution {
     
         boolean[] visited = new boolean[n];
         for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                if (i == j) continue;
-                if (computers[i][j] == 1 && !visited[i]) {
-                    dfs(n, visited, j, computers);
-                    answer++;
-                }
+            if (!visited[i]) {
+                dfs(n, visited, i, computers);
+                answer++;
             }
         }
-        
-        for (int i = 0; i < n; i++) {
-            if (!visited[i]) answer++;
-        }
-        
+    
         return answer;
     }
     
     private void dfs(int n, boolean[] visited, int visitIdx, int[][] computers) {
+        visited[visitIdx] = true;
         for (int i = 0; i < n; i++) {
             if (visited[i]) continue;
             if (computers[visitIdx][i] == 0) continue;
-            visited[i] = true;
             dfs(n, visited, i, computers);
         }
     }
