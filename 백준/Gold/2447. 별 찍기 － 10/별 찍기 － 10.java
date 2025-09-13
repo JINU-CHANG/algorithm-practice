3367564e4b189b55e4f9b2f9e298ac4e26e1128c
jinwoo22 <98975580+JINU-CHANG@users.noreply.github.com>
2025-09-13 13:31:27 +0900

    [Gold V] Title: 별 찍기 - 10, Time: 240 ms, Memory: 63976 KB -BaekjoonHub

--- 백준/Gold/2447. 별 찍기 － 10/별 찍기 － 10.java
+++ 백준/Gold/2447. 별 찍기 － 10/별 찍기 － 10.java
@@ -1,40 +1,24 @@
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
 
-    static char[][] map;
-    static char[][] nmap;
+    static boolean[][] checked;
+    static int[] dy = {0, 1, 2};
+    static int[] dx = {0, 1, 2};
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
 
         int n = Integer.parseInt(br.readLine());
-        map = new char[3][3];
+        checked = new boolean[n + 1][n + 1];
+        recursive(1, 1, n);
 
-        // 3일때 초기 세팅
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
-                if (i % 2 == 0) {
-                    map[i][j] = '*';
-                } else if (j % 2 == 0) {
-                    map[i][j] = '*';
-                } else {
-                    map[i][j] = ' ';
-                }
-
-            }
-        }
-
-        for (int i = 9; i <= n; i = i * 3) {
-            draw(i);
-        }
-
-        for (int i = 0; i < map.length; i++) {
-            for (int j = 0; j < map.length; j++) {
-                sb.append(map[i][j]);
+        for (int i = 1; i < n + 1; i++) {
+            for (int j = 1; j < n + 1; j++) {
+                if (!checked[i][j]) sb.append(" ");
+                else sb.append("*");
             }
             sb.append("\n");
         }
@@ -42,24 +26,29 @@ public class Main {
         System.out.println(sb);
     }
 
-    public static void draw(int x) {
-        nmap = new char[x][x];
-        for (int i = 0; i < x; i++) {
-            for (int j = 0; j < x; j++) {
-                if (( x / 3 <= i && i < (x / 3) * 2) && (x / 3 <= j && j < (x / 3) * 2)) {
-                    nmap[i][j] = ' ';
-                    continue;
-                }
-
-                int ni = i % (x / 3);
-                int nj = j % (x / 3);
-
-                nmap[i][j] = map[ni][nj];
+    private static void recursive(int y, int x, int size) {
+        if (size == 3) {
+            for (int i = 0; i < 3; i++) {
+                checked[y][x + i] = true;
             }
+
+            checked[y + 1][x] = true;
+            checked[y + 1][x + 2] = true;
+
+            for (int i = 0; i < 3; i++) {
+                checked[y + 2][x + i] = true;
+            }
+            return;
         }
 
-        map = nmap;
+        int nsize = size / 3;
+
+        for (int i = 0; i < 3; i++) {
+            int ny = y + (dy[i] * nsize);
+            for (int j = 0; j < 3; j++) {
+                if (i == 1 && j == 1) continue;
+                recursive(ny, x + (dx[j] * nsize) , nsize);
+            }
+        }
     }
-
 }
-
