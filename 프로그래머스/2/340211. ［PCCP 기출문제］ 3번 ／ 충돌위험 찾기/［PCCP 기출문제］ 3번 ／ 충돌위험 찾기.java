79bcebdb80105168b76646436e50f825eca697a2
jinwoo22 <98975580+JINU-CHANG@users.noreply.github.com>
2025-09-30 13:04:52 +0900

    [level 2] Title: [PCCP 기출문제] 3번 / 충돌위험 찾기, Time: 1126.32 ms, Memory: 348 MB -BaekjoonHub

--- 프로그래머스/2/340211. ［PCCP 기출문제］ 3번 ／ 충돌위험 찾기/［PCCP 기출문제］ 3번 ／ 충돌위험 찾기.java
+++ 프로그래머스/2/340211. ［PCCP 기출문제］ 3번 ／ 충돌위험 찾기/［PCCP 기출문제］ 3번 ／ 충돌위험 찾기.java
@@ -1,43 +1,66 @@
 import java.util.*;
 
-class Point {
-    int y;
-    int x;
+class Node {
+    Position position;
     int time;
 
-    public Point(int y, int x, int time) {
-        this.y = y;
-        this.x = x;
+    public Node(Position position, int time) {
+        this.position = position;
         this.time = time;
     }
 
     @Override
-    public boolean equals(Object object) {
-        if (this == object) {
-            return true;
-        }
-        if (object == null || getClass() != object.getClass()) {
-            return false;
-        }
-        Point point = (Point) object;
-        return y == point.y && x == point.x && time == point.time;
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Node node = (Node) o;
+        return position.equals(node.position) && time == node.time;
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(y, x, time);
+        return Objects.hash(position, time);
+    }
+}
+
+class Position {
+    int y;
+    int x;
+
+    Position(int y, int x) {
+        this.y = y;
+        this.x = x;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Position position = (Position) o;
+        return y == position.y && x == position.x;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(y, x);
     }
 }
 
 class Route {
-    List<Point> routes;
+    List<Node> routes;
 
-    Route (List<Point> routes) {
+    Route (List<Node> routes) {
         this.routes = routes;
     }
 
-    public void add(Point point) {
-        routes.add(point);
+    public int getTime() {
+        return routes.size();
+    }
+
+    public void add(Node node) {
+        routes.add(node);
     }
 }
 
@@ -52,75 +75,65 @@ class Solution {
         for (int i = 0; i < routes.length; i++) {
             int[] route = routes[i];
 
-            int startY = map.get(route[0])[0];
-            int startX = map.get(route[0])[1];
-            int endY = 0;
-            int endX = 0;
-            int time = 0;
+            Position start = new Position(map.get(route[0])[0], map.get(route[0])[1]);
+            Position end = new Position(0, 0);
             Route list = new Route(new ArrayList<>());
             for (int j = 1; j < route.length; j++) {
-                if (startY != endY && startX != endX) {
-                    list.add(new Point(startY, startX, time));
+                if (!start.equals(end)) {
+                    list.add(new Node(start, list.getTime()));
                 }
-                endY = map.get(route[j])[0];
-                endX = map.get(route[j])[1];
-                time = findRoute(startY, startX, endY, endX, list, time);
+                end = new Position(map.get(route[j])[0], map.get(route[j])[1]);
+                findRoute(start, end, list);
 
-                startY = endY;
-                startX = endX;
+                start = end;
             }
 
             results.add(list);
         }
 
-        // for (int i = 0; i < routes.length; i++) {
-        //     for(int[] arr : results.get(i).routes) {
-        //         System.out.println(Arrays.toString(arr));
-        //     }
-        // }
-
         return findCollision(results);
     }
 
-    private int findRoute(int startY, int startX, int endY, int endX, Route route, int time) {
-        int x = 1;
-        if (startY > endY) x = -1;
-        else x = 1;
+    private void findRoute(Position start, Position end, Route route) {
+        int y = start.y;
+        int x = start.x;
 
-        while (startY != endY) {
-            time++;
-            startY = startY + x;
-            Point point = new Point(startY, startX, time);
-            route.add(point);
-            if (startY == endY && startX == endX) return time;
+        while (y != end.y) {
+            int time = route.getTime();
+            if (y < end.y) y += 1;
+            else y  -= 1;
+
+            Position nPosition = new Position(y, x);
+            Node node = new Node(nPosition, time);
+            route.add(node);
+            if (nPosition.equals(start)) return;
         }
 
-        if (startX > endX) x = -1;
-        else x = 1;
-
-        while (startX != endX) {
-            time++;
-            startX = startX + x;
-            Point point = new Point(startY, startX, time);
-            route.add(point);
-            if (startX == endX) return time;
+        while (x != end.x) {
+            int time = route.getTime();
+            if (x < end.x) x += 1;
+            else x -= 1;
+            
+            Position nPosition = new Position(y, x);
+            Node node = new Node(nPosition, time);
+            route.add(node);
+            if (nPosition.equals(start)) return;
         }
-        return time;
     }
 
     private int findCollision(List<Route> results) {
-        Set<Point> points = new HashSet<>();
-        Set<Point> samePoints = new HashSet<>();
+        Set<Node> nodes = new HashSet<>();
+        Set<Node> sameNodes = new HashSet<>();
         for (Route result : results) {
-            List<Point> routes = result.routes;
+            List<Node> routes = result.routes;
             for (int i = 0; i < routes.size(); i++) {
-                Point point = routes.get(i);
-                if (points.contains(point)) {
-                    samePoints.add(point);
-                } else points.add(point);
+                Node node = routes.get(i);
+                if (nodes.contains(node)) {
+                    sameNodes.add(node);
+                } else nodes.add(node);
             }
         }
 
-        return samePoints.size();
+        return sameNodes.size();
     }
 }
