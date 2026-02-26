9a87a346fd5c05071bd5a10f311b8e513a5ba805
jinwoo22 <98975580+JINU-CHANG@users.noreply.github.com>
2026-02-26 12:41:32 +0900

    [Silver I] Title: 동물원, Time: 124 ms, Memory: 18596 KB -BaekjoonHub

---
+++ 백준/Silver/1309. 동물원/동물원.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        //StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(br.readLine());
+        long[][] dp = new long[n][3];
+
+        for (int i = 0; i < 3; i++) {
+            dp[0][i] = 1;
+        }
+
+        for (int i = 1; i < n; i++) {
+            dp[i][0] = (dp[i - 1][1] + dp[i - 1][2]) % 9901;
+            dp[i][1] = (dp[i - 1][0] + dp[i - 1][1] + dp[i - 1][2]) % 9901;
+            dp[i][2] = (dp[i - 1][0] + dp[i - 1][1]) % 9901;
+        }
+
+        long ans = 0;
+        for (int i = 0; i < 3; i++) {
+            ans += dp[n - 1][i] % 9901;
+        }
+
+        System.out.println(ans % 9901);
+    }
+}
