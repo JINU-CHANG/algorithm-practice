2f414e2b798cb02e53b17da9a48980319be84eaa
jinwoo22 <98975580+JINU-CHANG@users.noreply.github.com>
2026-02-28 21:45:29 +0900

    [Gold IV] Title: 미세먼지 안녕!, Time: 676 ms, Memory: 44884 KB -BaekjoonHub

--- 백준/Gold/17144. 미세먼지 안녕！/미세먼지 안녕！.java
+++ 백준/Gold/17144. 미세먼지 안녕！/미세먼지 안녕！.java
@@ -5,110 +5,107 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.StringTokenizer;
 
-class Main{
+public class Main {
 
-    static int R, C, T;
-    static int[][] map;
-    static int[] dy = {-1, 1, 0, 0};
-    static int[] dx = {0, 0, -1, 1};
-
-    public static void main(String[] args) throws IOException{
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
+        int R, C, T;
         R = Integer.parseInt(st.nextToken());
         C = Integer.parseInt(st.nextToken());
         T = Integer.parseInt(st.nextToken());
 
-        map = new int[R][C];
-        List<Integer> idxs = new ArrayList<>();
+        int[][] map = new int[R][C];
+        List<Integer> positions = new ArrayList<>();
         for (int i = 0; i < R; i++) {
             st = new StringTokenizer(br.readLine());
             for (int j = 0; j < C; j++) {
                 map[i][j] = Integer.parseInt(st.nextToken());
-                if (map[i][j] == -1) idxs.add(i);
+                if (map[i][j] == -1) positions.add(i);
             }
         }
 
-        for (int t = 0; t < T; t++) {
-
+        for (; T > 0; T--) {
             // 미세먼지 확산
-            int[][] temp = new int[R][C];
-            temp[idxs.get(0)][0] = -1;
-            temp[idxs.get(1)][0] = -1;
+            int[] dy = {-1, 1, 0, 0};
+            int[] dx = {0, 0, -1, 1};
+
+            int[][] nmap = new int[R][C];
+            for (int x : positions) {
+                nmap[x][0] = -1;
+            }
+
             for (int i = 0; i < R; i++) {
                 for (int j = 0; j < C; j++) {
-                    if (map[i][j] != 0 && map[i][j] != -1) {
+                    if (map[i][j] > 0) {
                         int count = 0;
                         for (int k = 0; k < 4; k++) {
-                            int ny = dy[k] + i;
-                            int nx = dx[k] + j;
+                            int ny = i + dy[k];
+                            int nx = j + dx[k];
 
                             if (ny >= R || nx >= C || ny < 0 || nx < 0) continue;
-                            if (nx == 0 && (ny == idxs.get(0) || ny == idxs.get(1))) continue;
-
-                            temp[ny][nx] += map[i][j] / 5;
+                            if (map[ny][nx] == -1) continue;
+                            nmap[ny][nx] += (map[i][j] / 5);
                             count++;
                         }
 
-                        temp[i][j] += (map[i][j] - (map[i][j] / 5) * count);
+                        nmap[i][j] += map[i][j] - ((map[i][j] / 5) * count);
                     }
                 }
             }
 
-            for (int i = 0; i < R; i++) {
-                for (int j = 0; j < C; j++) {
-                    map[i][j] = temp[i][j];
-                }
+            map = nmap;
+
+            int start = positions.get(0);
+            // 반시계 이동
+            for (int i = start - 2; i >= 0; i--) {
+                map[i + 1][0] = map[i][0];
             }
 
-            // 미세먼지 확산
-            int top = idxs.get(0);
-            for (int i = top - 1; i > 0; i--) {
-                map[i][0] = map[i - 1][0];
+            for (int i = 1; i < C; i++) {
+                map[0][i - 1] = map[0][i];
             }
 
-            for (int i = 0; i < C - 1; i++) {
-                map[0][i] = map[0][i + 1];
+            for (int i = 1; i <= start; i++) {
+                map[i - 1][C - 1] = map[i][C - 1];
             }
 
-            for (int i = 0; i < top; i++) {
-                map[i][C - 1] = map[i + 1][C - 1];
+            for (int i = C - 2; i >= 1; i--) {
+                map[start][i + 1] = map[start][i];
             }
 
-            for (int i = C - 1; i > 1; i--) {
-                map[top][i] = map[top][i - 1];
+            map[start][1] = 0;
+
+            // 시계 이동
+            start = positions.get(1);
+            for (int i = start + 2; i < R; i++) {
+                map[i - 1][0] = map[i][0];
             }
 
-            map[top][1] = 0;
-
-            int bottom = idxs.get(1);
-            for (int i = bottom + 1; i < R - 1; i++) {
-                map[i][0] = map[i + 1][0];
+            for (int i = 1; i < C; i++) {
+                map[R - 1][i - 1] = map[R - 1][i];
             }
 
-            for (int i = 0; i < C - 1; i++) {
-                map[R - 1][i] = map[R - 1][i + 1];
+            for (int i = R - 2; i >= start; i--) {
+                map[i + 1][C - 1] = map[i][C - 1];
             }
 
-            for (int i = R - 1; i > bottom; i--) {
-                map[i][C - 1] = map[i - 1][C - 1];
+            for (int i = C - 2; i >= 1; i--) {
+                map[start][i + 1] = map[start][i];
             }
 
-            for (int i = C - 1; i > 1; i--) {
-                map[bottom][i] = map[bottom][i - 1];
-            }
-
-            map[bottom][1] = 0;
+            map[start][1] = 0;
         }
 
-        int answer = 0;
+        int sum = 0;
         for (int i = 0; i < R; i++) {
             for (int j = 0; j < C; j++) {
-                answer += map[i][j];
+                if (map[i][j] == -1) continue;
+                sum += map[i][j];
             }
         }
 
-        System.out.println(answer + 2);
+        System.out.println(sum);
     }
 }
