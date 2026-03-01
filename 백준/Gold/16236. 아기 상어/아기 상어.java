db7047d8311d21e0bd3fbec5cf58372d6f4a0f56
jinwoo22 <98975580+JINU-CHANG@users.noreply.github.com>
2026-03-01 10:58:26 +0900

    [Gold III] Title: 아기 상어, Time: 108 ms, Memory: 14776 KB -BaekjoonHub

--- 백준/Gold/16236. 아기 상어/아기 상어.java
+++ 백준/Gold/16236. 아기 상어/아기 상어.java
@@ -1,21 +1,24 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.LinkedList;
-import java.util.PriorityQueue;
+import java.util.List;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
 
     static int n;
-    static int sharkSize = 2;
-    static int count = 0;
-    static int[] currentPosition;
-    static int time = 0;
+    static int[] dy = {-1, 1, 0, 0};
+    static int[] dx = {0, 0, -1, 1};
     static int[][] map;
-    static int[] dy = {-1, 0, 1, 0}; // 상, 좌, 하, 우
-    static int[] dx = {0, -1, 0, 1}; // 상, 좌, 하, 우
+    static boolean[][] visited;
+    static int size = 2;
+    static int count = 0;
+    static int y, x;
+    static int ans = 0;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -23,50 +26,53 @@ public class Main {
 
         n = Integer.parseInt(st.nextToken());
         map = new int[n][n];
+        visited = new boolean[n][n];
         for (int i = 0; i < n; i++) {
             st = new StringTokenizer(br.readLine());
             for (int j = 0; j < n; j++) {
                 map[i][j] = Integer.parseInt(st.nextToken());
                 if (map[i][j] == 9) {
-                    currentPosition = new int[]{i, j};
+                    y = i;
+                    x = j;
                     map[i][j] = 0;
                 }
             }
         }
 
         while (true) {
-            int[] result = findFish(currentPosition);
-            if (result == null) break;
-
-           // System.out.println(Arrays.toString(currentPosition));
-            map[result[0]][result[1]] = 0;
-            count++;
-            if (count == sharkSize) {
-                sharkSize++;
-                count = 0;
+            visited = new boolean[n][n];
+            int[] result = bfs(y, x);
+            if (result == null) {
+                System.out.println(ans);
+                break;
             }
 
-            time += result[2];
-            currentPosition = result;
+            //System.out.println(Arrays.toString(result));
+            y = result[0];
+            x = result[1];
+            map[y][x] = 0;
+            count++;
+            if (size == count) {
+                size++;
+                count = 0;
+            }
+            ans += result[2];
         }
-
-        System.out.println(time);
     }
 
-    private static int[] findFish(int[] currentPosition) {
-        Queue<int[]> distQueue = new LinkedList<>();
-        Queue<int[]> fishQueue = new PriorityQueue<>((o1, o2) -> {
-            if (o1[0] == o2[0]) return o1[1] - o2[1];
-            else return o1[0] - o2[0];
-        });
+    private static int[] bfs(int y, int x) {
+        int result = -1;
+        List<int[]> results = new ArrayList<>();
+        Queue<int[]> queue = new LinkedList<>();
+        visited[y][x] = true;
+        queue.add(new int[]{y, x, 0});
 
-        int min = Integer.MAX_VALUE;
-        boolean[][] visited = new boolean[n][n];
-        distQueue.offer(new int[]{currentPosition[0], currentPosition[1], 0});
-        visited[currentPosition[0]][currentPosition[1]] = true;
+        while (!queue.isEmpty()) {
+            int[] polled = queue.poll();
 
-        while (!distQueue.isEmpty()) {
-            int[] polled = distQueue.poll();
+            if (result != -1 && polled[2] > result) {
+                break;
+            }
 
             for (int i = 0; i < 4; i++) {
                 int ny = polled[0] + dy[i];
@@ -74,20 +80,29 @@ public class Main {
 
                 if (ny >= n || nx >= n || ny < 0 || nx < 0) continue;
                 if (visited[ny][nx]) continue;
-                if (map[ny][nx] > sharkSize) continue; // 자신보다 큰 물고기 있는 칸 지날 수 없음
-                if (polled[2] >= min) break;
+                if (map[ny][nx] > size) continue;
 
-                if (0 < map[ny][nx] && map[ny][nx] < sharkSize) {
-                    fishQueue.add(new int[]{ny, nx, polled[2] + 1});
-                    min = polled[2] + 1;
+                if (result == -1 && map[ny][nx] > 0 && map[ny][nx] < size) {
+                    result = polled[2];
+                }
+                if (result == polled[2] && map[ny][nx] > 0 && map[ny][nx] < size) {
+                    results.add(new int[]{ny, nx, polled[2] + 1});
                 }
 
                 visited[ny][nx] = true;
-                distQueue.offer(new int[]{ny, nx, polled[2] + 1});
+                queue.add(new int[]{ny, nx, polled[2] + 1});
             }
         }
 
-        if (fishQueue.isEmpty()) return null;
-        return fishQueue.poll();
+        if (results.isEmpty()) {
+            return null;
+        }
+
+        Collections.sort(results, (a1, a2) -> {
+            if (a1[0] == a2[0]) return a1[1] - a2[1];
+            else return (a1[0] - a2[0]);
+        });
+
+        return results.get(0);
     }
 }
