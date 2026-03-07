26b336be306867f718edbff9189c0a068c8dca9d
jinwoo22 <98975580+JINU-CHANG@users.noreply.github.com>
2026-03-07 10:34:52 +0900

    [Silver I] Title: 신입 사원, Time: 3020 ms, Memory: 312724 KB -BaekjoonHub

--- 백준/Silver/1946. 신입 사원/신입 사원.java
+++ 백준/Silver/1946. 신입 사원/신입 사원.java
@@ -1,46 +1,44 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Stack;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
 
-    static int t, n;
-    static int[] array;
-    static Stack<Integer> stack;
-
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, InterruptedException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         StringBuilder sb = new StringBuilder();
 
-        t = Integer.parseInt(st.nextToken());
-        for (int k = 0; k < t; k++) {
-            n = Integer.parseInt(br.readLine());
-            array = new int[n + 1];
-            for (int i = 1; i < n + 1; i++) {
-                st = new StringTokenizer(br.readLine());
-                int x = Integer.parseInt(st.nextToken());
-                int y = Integer.parseInt(st.nextToken());
+        int T = Integer.parseInt(st.nextToken());
 
-                array[x] = y;
+        for (; T > 0; T--) {
+            int n = Integer.parseInt(br.readLine());
+            int[][] arr = new int[n][2];
+            for (int i = 0; i < n; i++) {
+                st = new StringTokenizer(br.readLine());
+                arr[i] = new int[]{Integer.parseInt(st.nextToken()),
+                        Integer.parseInt(st.nextToken())};
             }
 
-            sb.append(solve()).append("\n");
+            Arrays.sort(arr, (a1, a2) -> {
+                return a1[0] - a2[0];
+            });
+
+            int min = Integer.MAX_VALUE;
+            int ans = 0;
+            for (int i = 0; i < n; i++) {
+                if(arr[i][1] < min) {
+                    ans++;
+                    min = arr[i][1];
+                }
+            }
+
+            sb.append(ans).append("\n");
+//            System.out.println(Arrays.toString(arr[0]));
         }
 
         System.out.println(sb);
-    }
-
-    public static int solve() {
-        Stack<Integer> stack = new Stack<>();
-        stack.push(array[1]);
-        for (int i = 2; i < n + 1; i++) {
-            if (stack.peek() < array[i]) continue;
-            stack.push(array[i]);
-        }
-
-        return stack.size();
     }
 }
